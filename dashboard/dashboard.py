63328a089e60142f423b7b546e0f7a6a7e430254
cramer-rao <vionaholika156@gmail.com>
2024-10-04 21:01:57 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -47,7 +47,7 @@ def create_rfm_df(df):
 
 dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(dir, 'all_data.csv')
-all_df = pd.read_csv(filepath)
+all_df = pd.read_csv(file_path)
 
 datetime_columns = ["order_date", "delivery_date"]
 all_df.sort_values(by="order_date", inplace=True)
